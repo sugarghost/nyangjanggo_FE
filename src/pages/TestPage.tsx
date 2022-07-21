0c8943180d6994eca48eed3051e17ea52f93d3c3
sugarghost <code.sugartoast@gmail.com>
2022-07-21 21:17:21 +0900

    토큰 테스트를 위한 commit

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,15 +1,45 @@
+import axios from 'axios';
 import React, { ChangeEvent, InputHTMLAttributes, Suspense, useEffect, useState } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import userToken from '../recoil/userAtom';
+import { getToken } from '../utils/jwt';
 
 function TestPage({}) {
+  /*
   const setUserToken = useSetRecoilState(userToken);
   setUserToken(
     // eslint-disable-next-line max-len
     'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJodHNsdHM5NUBnbWFpbC5jb20iLCJyb2xlcyI6IlVTRVIiLCJpYXQiOjE2NTc1NDIzOTMsImV4cCI6MTY1ODc1MTk5M30.IVoFRLjsMx_TevanNPKkJRoPzlXXxffPju1gzCn6ato',
   );
-  return <div />;
+  */
+  const testInstance = axios.create();
+
+  testInstance.defaults.validateStatus = (status) => status < 400;
+  testInstance.defaults.baseURL = 'https://api.nyangjanggo.com/refresh';
+  testInstance.defaults.timeout = 30000;
+  testInstance.defaults.withCredentials = true;
+
+  const accessToken = getToken();
+
+  const token = () => {
+    testInstance
+      .get(
+        `https://api.nyangjanggo.com/refresh`, // token refresh api
+        { headers: { 'Access-Token': `${accessToken}` }, withCredentials: true },
+      )
+      .then((result) => {
+        console.log('result :', result);
+      })
+      .catch((refreshError) => {
+        console.log('refreshError: ', refreshError);
+      });
+  };
+  return (
+    <div>
+      <button onClick={token}> 토큰 테스트 </button>
+    </div>
+  );
 }
 
 export default TestPage;
