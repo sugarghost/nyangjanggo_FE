6304829fb1171addb041d62cb2252d6016c8bb11
sugarghost <code.sugartoast@gmail.com>
2022-07-26 13:52:25 +0900

    main페이지 Search 분리 작업중

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,20 @@
+import EntitySearched from '@components/search/EntitySearched';
 import MainSearched from '@components/search/MainSearched';
 import Search from '@components/search/Search';
+import { searchQuery } from '@recoil/searchAtom';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 
 const MainPage = () => {
   const navigate = useNavigate();
+  const searchedType = useRecoilValue(searchQuery);
+
   return (
     <>
       <Search />
       {/* Suspense를 사용하면 컴포넌트가 렌더링되기 전까지 기다릴 수 있습니다 */}
-      <MainSearched />
+      {searchedType.type === '' ? <MainSearched /> : <EntitySearched />}
     </>
   );
 };
