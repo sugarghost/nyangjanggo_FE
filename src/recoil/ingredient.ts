7873539015b8cc47fb9e3c79ffd30458dc85b5ac
sugarghost <code.sugartoast@gmail.com>
2022-07-26 17:04:26 +0900

    냉장고 재료명만 가져오는 Selector 추가

--- src/recoil/ingredient.ts
+++ src/recoil/ingredient.ts
@@ -1,15 +1,22 @@
-import { getResource } from '@/apis/ResourceApi';
+import { getResource, Ingredient } from '@/apis/ResourceApi';
 import { authInstance } from '@/apis/axiosInstance';
-import { Ingredient } from '@/apis/ResourceApi';
 import { selector, useRecoilValue, atom, selectorFamily } from 'recoil';
-
 
 export const ingredientsSelector = selector({
   key: 'userSelector',
   get: async () => {
     const response = await getResource();
-    const data = response.data ? response.data : [] as unknown as Ingredient[];
+    const data = response.data ? response.data : ([] as unknown as Ingredient[]);
     return data;
   },
 });
 
+export const ingredientsNameSelector = selector({
+  key: 'userSelector',
+  get: async () => {
+    const response = await getResource();
+    const data = [];
+    response.data.map((value) => data.push(value.resourceName));
+    return data;
+  },
+});
